f5fc96c7075dc31c1c782d2c796c7dda9f4bc0e7
Andersonps-dev <aaandersonpereira45265@gmail.com>
2025-03-03 15:50:26 -0300

    `Refactor AIBot to use LangChain Chroma and update webhook endpoint to handle conversation history`

--- app.py
+++ app.py
@@ -7,35 +7,33 @@ app = Flask(__name__)
 @app.route('/chatbot/webhook/', methods=['POST'])
 def webhook():
     data = request.json
-
-    if 'payload' not in data:
-        return jsonify({'status': 'error', 'message': 'Missing payload in request'}), 400
-
-    payload = data['payload']
-
-    if 'from' not in payload or 'body' not in payload:
-        return jsonify({'status': 'error', 'message': 'Missing required fields in payload'}), 400
-
-    chat_id = payload['from']
-    received_message = payload['body']
+    chat_id = data['payload']['from']
+    received_message = data['payload']['body']
     is_group = '@g.us' in chat_id
-    is_status = 'status@broadcast' in chat_id
 
-    if is_group or is_status:
-        return jsonify({'status': 'success', 'message': 'Mensagem de grupo/status ignorada.'}), 200
+    if is_group:
+        return jsonify({'status': 'success', 'message': 'Mensagem de grupo ignorada.'}), 200
 
     waha = Waha()
     ai_bot = AIBot()
 
     waha.start_typing(chat_id=chat_id)
-    response = ai_bot.invoke(question=received_message)
+    history_messages = waha.get_history_messages(
+        chat_id=chat_id,
+        limit=10,
+    )
+    response_message = ai_bot.invoke(
+        history_messages=history_messages,
+        question=received_message,
+    )
     waha.send_message(
         chat_id=chat_id,
-        message=response,
+        message=response_message,
     )
     waha.stop_typing(chat_id=chat_id)
 
     return jsonify({'status': 'success'}), 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
