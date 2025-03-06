188cede69cad6460b9f304a1f460333cda105c46
Andersonps-dev <aaandersonpereira45265@gmail.com>
2025-03-06 09:24:24 -0300

    `Refactor start-waha.sh script: remove commented lines and simplify`

--- start-waha.sh
+++ start-waha.sh
@@ -7,19 +7,14 @@ then
     apt-get update && apt-get install -y dumb-init
 fi
 
-# Inicia o Waha normalmente
 /usr/bin/dumb-init -- node /app/build/index.js &
 
-# Aguarda alguns segundos para o servidor iniciar
 sleep 10
 
-# Inicia a sessão automaticamente
 curl -X POST http://waha:3000/api/sessions/default/start
 
-# Aguarda mais alguns segundos para garantir que a sessão foi criada
 sleep 5
 
-# Registra o webhook automaticamente
 curl -X POST http://localhost:3000/api/sessions/default/webhook \
   -H "Content-Type: application/json" \
   -d '{
@@ -27,5 +22,4 @@ curl -X POST http://localhost:3000/api/sessions/default/webhook \
     "events": ["message"]
   }'
 
-# Mantém o processo rodando
 wait
