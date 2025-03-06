877c4b494f12cd15bdfb861768983b971bc3b88e
Andersonps-dev <aaandersonpereira45265@gmail.com>
2025-03-06 08:42:07 -0300

    `Updated app.py to change limit from 3 to 5 in webhook function. Updated binary files in chroma_data directory.`

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def webhook():
     waha.start_typing(chat_id=chat_id)
     history_messages = waha.get_history_messages(
         chat_id=chat_id,
-        limit=3,
+        limit=5,
     )
     response_message = ai_bot.invoke(
         history_messages=history_messages,
